9ad59fbf864f8929b6f7d40d4d41e5120e4b37d0
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-06-24 21:28:07 -0700

    Added scripts to process Campbell brain data.

---
+++ inst/scripts/make-campbell-brain-metadata.R
@@ -0,0 +1,24 @@
+write.csv(file="../extdata/metadata-campbell-brain.csv", 
+    data.frame(
+        Title = sprintf("Campbell brain %s", c("counts", "colData")),
+        Description = sprintf("%s for the Campbell brain single-cell RNA-seq dataset", 
+            c("Count matrix", "Per-cell metadata")),
+        RDataPath = file.path("scRNAseq", "campbell-brain", "2.0.0", 
+            c("counts.rds", "coldata.rds")),
+        BiocVersion="3.10",
+        Genome="mm10",
+        SourceType="TXT",
+        SourceUrl="https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE93374",
+        SourceVersion=c(
+            "GSE93374_Merged_all_020816_DGE.txt.gz",
+            "GSE93374_cell_metadata.txt.gz"),
+        Species="Mus musculus",
+        TaxonomyId="10090",
+        Coordinate_1_based=TRUE,
+        DataProvider="Linus Tzu-Yen Tsai",
+        Maintainer="Aaron Lun <infinite.monkeys.with.keyboards@gmail.com>",
+        RDataClass="character",
+        DispatchClass="Rds",
+        stringsAsFactors = FALSE
+    ),
+    row.names=FALSE)
