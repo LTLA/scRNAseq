143fd352055ae496118f2a89d29fc3aecb6a3946
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-06-28 23:02:32 -0700

    Added scripts to prepare the Xin pancreas data.

---
+++ inst/scripts/make-xin-pancreas-metadata.R
@@ -0,0 +1,26 @@
+write.csv(file="../extdata/metadata-xin-pancreas.csv",
+    data.frame(
+        Title = sprintf("Xin pancreas %s", c("RPKMs", "rowData", "colData")),
+        Description = sprintf("%s for the Xin pancreas single-cell RNA-seq dataset", 
+            c("RPKM matrix", "Per-gene metadata", "Per-cell metadata")),
+        RDataPath = file.path("scRNAseq", "xin-pancreas", "2.0.0", 
+            c("rpkm.rds", "rowdata.rds", "coldata.rds")),
+        BiocVersion="3.10",
+        Genome="hg19",
+        SourceType=c("TSV", "CSV", "TSV"),
+        SourceUrl=c("https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE81608",
+            rep("https://s3.amazonaws.com/scrnaseq-public-datasets/manual-data/xin", 2)), 
+        SourceVersion=c(
+            "GSE81608_human_islets_rpkm.txt.gz",
+            "human_gene_annotation.csv",
+            "human_islet_cell_identity.txt"),
+        Species="Homo sapiens",
+        TaxonomyId="9606",
+        Coordinate_1_based=TRUE,
+        DataProvider="Yurong Xin",
+        Maintainer="Aaron Lun <infinite.monkeys.with.keyboards@gmail.com>",
+        RDataClass="character",
+        DispatchClass="Rds",
+        stringsAsFactors = FALSE
+    ),
+    row.names=FALSE)
