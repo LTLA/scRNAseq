35fde14938fbbe80430bd3a2c71e27d566d7db08
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-06-29 02:06:10 -0700

    Added R code to obtain the Muraro, Grun datasets.

---
+++ R/GrunPancreasData.R
@@ -0,0 +1,37 @@
+#' Obtain the Grun pancreas data
+#'
+#' Download and cache the Grun pancreas single-cell RNA-seq (scRNA-seq) dataset from ExperimentHub,
+#' returning a \linkS4class{SingleCellExperiment} object for further use.
+#'
+#' @details
+#' This function provides the human pancreas scRNA-seq data from Grun et al. (2016)
+#' in the form of a \linkS4class{SingleCellExperiment} object with a single matrix of UMI counts. 
+#'
+#' Row data contains fields for the symbol and chromosomal location of each gene.
+#' Spike-ins are specially labelled with the \code{\link{isSpike}} function.
+#'
+#' Column metadata is derived from the columns of the count matrix provided in GSE85241,
+#' with additional cell type labels obtained from the authors (indirectly, via the Hemberg group) 
+#'
+#' @return A \linkS4class{SingleCellExperiment} object.
+#'
+#' @author Aaron Lun,
+#' using additional metadata obtained by Vladimir Kiselev.
+#'
+#' @references
+#' Grun MJ et al. (2016). 
+#' De novo prediction of stem cell identity using single-cell transcriptome data. 
+#' \emph{Cell Stem Cell} 19(2), 266-277. 
+#'
+#' @examples
+#' sce <- GrunPancreasData()
+#' 
+#' @export
+#' @importFrom SummarizedExperiment rowData
+#' @importFrom SingleCellExperiment isSpike<-
+GrunPancreasData <- function() {
+    version <- "2.0.0"
+    sce <- .create_sce(file.path("grun-pancreas", version)) 
+    isSpike(sce, "ERCC") <- grep("ERCC-[0-9]+", rowData(sce)$symbol)
+    sce
+}
