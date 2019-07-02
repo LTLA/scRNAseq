0d24a55495eeccfdfa9f839d04cc0ddc20a1cf65
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-07-01 20:58:18 -0700

    Added scripts to prepare the Macosko retina data.

---
+++ inst/scripts/make-macosko-retina-metadata.R
@@ -0,0 +1,27 @@
+write.csv(file="../extdata/metadata-macosko-retina.csv", 
+    data.frame(
+        Title = sprintf("Macosko retina %s", c("counts", "colData")),
+        Description = sprintf("%s for the Macosko retina single-cell RNA-seq dataset", 
+            c("Count matrix", "Per-cell metadata")),
+        RDataPath = file.path("scRNAseq", "macosko-retina", "2.0.0", 
+            c("counts.rds", "coldata.rds")),
+        BiocVersion="3.10",
+        Genome="mm10",
+        SourceType="TSV",
+        SourceUrl=c(
+            "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE63472",
+            "http://mccarrolllab.com/wp-content/uploads/2015/05"
+        ),
+        SourceVersion=c(
+            "GSE63472_P14Retina_merged_digital_expression.txt.gz",
+            "retina_clusteridentities.txt"),
+        Species="Mus musculus",
+        TaxonomyId="10090",
+        Coordinate_1_based=TRUE,
+        DataProvider="Evan Macosko",
+        Maintainer="Aaron Lun <infinite.monkeys.with.keyboards@gmail.com>",
+        RDataClass="character",
+        DispatchClass="Rds",
+        stringsAsFactors = FALSE
+    ),
+    row.names=FALSE)
