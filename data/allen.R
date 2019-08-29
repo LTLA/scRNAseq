c01ee890d8729a45fb584f7df2430a7ff2549138
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-08-28 22:51:20 -0700

    Hide data() execution details within function scope.

--- data/allen.R
+++ data/allen.R
@@ -1,6 +1,8 @@
 .Deprecated(msg="'data(allen)' is deprecated.\nUse ReprocessedAllenData() instead.")
-bfc <- BiocFileCache::BiocFileCache(ask=FALSE)
-bpath <- BiocFileCache::bfcrpath(bfc, "https://github.com/LTLA/scRNAseq/raw/80b3d4dde812b72406499922c5cd157e7b8d3b45/data/allen.rda")
-env <- new.env()
-load(bpath, envir=env)
-allen <- env$allen
+allen <- (function(){
+    bfc <- BiocFileCache::BiocFileCache(ask=FALSE)
+    bpath <- BiocFileCache::bfcrpath(bfc, "https://github.com/LTLA/scRNAseq/raw/80b3d4dde812b72406499922c5cd157e7b8d3b45/data/allen.rda")
+    env <- new.env()
+    load(bpath, envir=env)
+    env$allen
+})()
--- data/fluidigm.R
+++ data/fluidigm.R
@@ -1,6 +1,8 @@
 .Deprecated(msg="'data(fluidigm)' is deprecated.\nUse ReprocessedFluidigmData() instead.")
-bfc <- BiocFileCache::BiocFileCache(ask=FALSE)
-bpath <- BiocFileCache::bfcrpath(bfc, "https://github.com/LTLA/scRNAseq/raw/80b3d4dde812b72406499922c5cd157e7b8d3b45/data/fluidigm.rda")
-env <- new.env()
-load(bpath, envir=env)
-fluidigm <- env$fluidigm
+fluidigm <- (function(){
+    bfc <- BiocFileCache::BiocFileCache(ask=FALSE)
+    bpath <- BiocFileCache::bfcrpath(bfc, "https://github.com/LTLA/scRNAseq/raw/80b3d4dde812b72406499922c5cd157e7b8d3b45/data/fluidigm.rda")
+    env <- new.env()
+    load(bpath, envir=env)
+    env$fluidigm
+})()
