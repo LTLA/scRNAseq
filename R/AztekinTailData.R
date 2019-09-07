e5131b2bfb2b1eb1088e34d498f8d78cf18392c3
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-09-07 03:01:09 -0700

    Added R code to pull the Aztekin dataset.

---
+++ R/AztekinTailData.R
@@ -0,0 +1,37 @@
+#' Obtain the Aztekin tail data
+#'
+#' Obtain the Xenopus tail single-cell RNA-seq data from Aztekin et al. (2019).
+#'
+#' @details
+#' Column metadata is provided in the same form as supplied in E-MTAB-7761.
+#' This contains information such as the treatment condition, batch, putative cell type, putative cell cycle phase.
+#'
+#' The UMAP results are available in the reduced dimensions field of the output object.
+#'
+#' All data are downloaded from ExperimentHub and cached for local re-use.
+#' Specific resources can be retrieved by searching for \code{scRNAseq/aztekin-tail}.
+#' 
+#' @return A \linkS4class{SingleCellExperiment} object with a single matrix of UMI counts.
+#'
+#' @author Aaron Lun
+#'
+#' @references
+#' Aztekin C et al. (2019).
+#' Identification of a regeneration-organizing cell in the Xenopus tail.
+#' \emph{Science} 364(6441), 653-658
+#'
+#' @examples
+#' sce <- AztekinTailData()
+#' 
+#' @export
+#' @importFrom SingleCellExperiment splitAltExps reducedDim<- colData colData<-
+AztekinTailData <- function() {
+    version <- "2.0.0"
+    sce <- .create_sce(file.path("aztekin-tail", version), has.rowdata=FALSE)
+
+    # Move UMAP coordinates to the reducedDims.
+    reducedDim(sce, "UMAP") <- as.matrix(colData(sce)[,c("X", "Y")])
+    colData(sce) <- colData(sce)[,setdiff(colnames(sce), c("X", "Y"))]
+
+    sce
+}
