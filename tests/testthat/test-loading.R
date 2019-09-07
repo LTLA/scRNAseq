3e8e59b6b03a16b39e6594dca6aef33cb59720a9
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-09-06 22:16:01 -0700

    Added tests for alternative options.

---
+++ tests/testthat/test-loading.R
@@ -0,0 +1,87 @@
+# This checks through all of the getter functions.
+# Note that we don't re-run any of the code in the Examples.
+# library(testthat); library(scRNAseq); source("test-loading.R")
+
+CHECK <- function(sce) {
+    expect_true(all(dim(sce) > 0))
+    expect_true(length(assayNames(sce)) > 0)
+}
+
+test_that("BaronPancreasData works", {
+    CHECK(BaronPancreasData(ensembl=TRUE))
+    CHECK(BaronPancreasData("mouse", ensembl=TRUE))
+})
+
+test_that("CampbellBrainData works", {
+    CHECK(CampbellBrainData(ensembl=TRUE))
+})
+
+test_that("ChenBrainData works", {
+    CHECK(ChenBrainData(ensembl=TRUE))
+})
+
+test_that("GrunHSCData works", {
+    CHECK(GrunHSCData(ensembl=TRUE))
+})
+
+test_that("GrunPancreasData works", {
+    CHECK(GrunPancreasData(ensembl=TRUE))
+})
+
+test_that("LaMannoBrainData works", {
+    CHECK(LaMannoBrainData(ensembl=TRUE, "human-es"))
+    CHECK(LaMannoBrainData(ensembl=TRUE, "human-embryo"))
+    CHECK(LaMannoBrainData(ensembl=TRUE, "human-ips"))
+    CHECK(LaMannoBrainData(ensembl=TRUE, "mouse-adult"))
+    CHECK(LaMannoBrainData(ensembl=TRUE, "mouse-embryo"))
+})
+
+test_that("MacoskoRetinaData works", {
+    CHECK(MacoskoRetinaData(ensembl=TRUE))
+})
+
+test_that("MarquesBrainData works", {
+    CHECK(MarquesBrainData(ensembl=TRUE))
+})
+
+test_that("MuraroPancreasData works", {
+    CHECK(MuraroPancreasData(ensembl=TRUE))
+})
+
+test_that("NestorowaHSCData works", {
+    CHECK(NestorowaHSCData(remove.htseq=FALSE))
+})
+
+test_that("Reprocessed*Data works", {
+    CHECK(ReprocessedAllenData(assays="tophat_counts", ensembl=TRUE))
+    CHECK(ReprocessedTh2Data(assays="tophat_counts", ensembl=TRUE))
+    CHECK(ReprocessedFluidigmData(assays="tophat_counts", ensembl=TRUE))
+})
+
+test_that("RomanovBrainData works", {
+    CHECK(RomanovBrainData(ensembl=TRUE))
+})
+
+test_that("SegerstolpePancreasData works", {
+    CHECK(SegerstolpePancreasData(ensembl=TRUE))
+})
+
+test_that("ShekharRetinaData works", {
+    CHECK(ShekharRetinaData(ensembl=TRUE))
+})
+
+test_that("TasicBrainData works", {
+    CHECK(TasicBrainData(ensembl=TRUE))
+})
+
+test_that("UsoskinBrainData works", {
+    CHECK(UsoskinBrainData(ensembl=TRUE))
+})
+
+test_that("XinPancreasData works", {
+    CHECK(XinPancreasData(ensembl=TRUE))
+})
+
+test_that("ZeiselBrainData works", {
+    CHECK(ZeiselBrainData(ensembl=TRUE))
+})
