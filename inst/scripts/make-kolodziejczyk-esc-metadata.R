b102b8bf8dd6492e6615c01dcfb1437ee2199df1
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-09-07 01:55:45 -0700

    Added prep and metadata scripts for Kolod ESC data.

---
+++ inst/scripts/make-kolodziejczyk-esc-metadata.R
@@ -0,0 +1,21 @@
+write.csv(file="../extdata/metadata-kolodziejczyk-esc.csv", 
+    data.frame(
+        Title = "Kolodziejczyk mouse ESC counts",
+        Description = "Count matrix for the Kolodziejczyk mouse embryonic stem cell single-cell RNA-seq dataset", 
+        RDataPath = file.path("scRNAseq", "kolodziejczyk-esc", "2.0.0", "counts.rds"),
+        BiocVersion="3.10",
+        Genome="mm10",
+        SourceType="TXT",
+        SourceUrl="https://espresso.teichlab.sanger.ac.uk/static",
+        SourceVersion="counttable_es.csv",
+        Species="Mus musculus",
+        TaxonomyId="10090",
+        Coordinate_1_based=NA,
+        DataProvider="Sarah Teichmann",
+        Maintainer="Aaron Lun <infinite.monkeys.with.keyboards@gmail.com>",
+        RDataClass="matrix", 
+        DispatchClass="Rds",
+        stringsAsFactors = FALSE
+    ),
+    row.names=FALSE
+)
