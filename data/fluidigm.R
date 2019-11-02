de554d0767a6d8f4f01c5ca85f8103c682f2cdaa
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-11-01 21:39:41 -0700

    Removed unnecessary data/ scripts.

--- data/fluidigm.R
+++
@@ -1,8 +0,0 @@
-.Deprecated(msg="'data(fluidigm)' is deprecated.\nUse ReprocessedFluidigmData() instead.")
-fluidigm <- (function(){
-    bfc <- BiocFileCache::BiocFileCache(ask=FALSE)
-    bpath <- BiocFileCache::bfcrpath(bfc, "https://github.com/LTLA/scRNAseq/raw/80b3d4dde812b72406499922c5cd157e7b8d3b45/data/fluidigm.rda")
-    env <- new.env()
-    load(bpath, envir=env)
-    env$fluidigm
-})()
