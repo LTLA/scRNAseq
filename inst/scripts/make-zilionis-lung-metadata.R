14273a4465e59a18e341521d18b2c5951160b51a
Jens Preussner <jens.preussner@mpi-bn.mpg.de>
2020-05-22 12:02:33 +0000

    Made requested changes

--- inst/scripts/make-zilionis-lung-metadata.R
+++ inst/scripts/make-zilionis-lung-metadata.R
@@ -3,7 +3,7 @@ FUN <- function(species, short, long, id) {
     Title = sprintf("Zilionis %s lung %s", species, c("counts", "colData")),
     Description = sprintf("%s for the Zilionis %s lung single-cell RNA-seq dataset", 
                           c("Count matrix", "Per-cell metadata"), species),
-    RDataPath = file.path("scRNAseq", "zilionis-lung", "2.2.0", 
+    RDataPath = file.path("scRNAseq", "zilionis-lung", "2.4.0", 
                           sprintf(c("counts-%s.rds", "coldata-%s.rds"), species)),
     BiocVersion="3.10",
     Genome=short,
@@ -21,6 +21,6 @@ FUN <- function(species, short, long, id) {
   )
 }
 
-write.csv(file="../extdata/2.2.0/metadata-zilionis-lung.csv", 
+write.csv(file="../extdata/2.4.0/metadata-zilionis-lung.csv", 
           rbind(FUN("human", "hg38", "Homo sapiens", "9606"), FUN("mouse", "mm10", "Mus musculus", "10090")),
           row.names=FALSE)
