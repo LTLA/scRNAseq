68b8e254306fbb439fca3fc18938fe1b14183332
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-05-29 22:51:16 -0700

    Cleaned up SPRING unpacking to reducedDims for Zilionis.

--- R/ZilionisLungData.R
+++ R/ZilionisLungData.R
@@ -17,7 +17,10 @@
 #' Note that this is only performed if \code{ensembl=TRUE}.
 #' 
 #' If \code{filter=TRUE}, only cells that have been used in the original analysis are returned.
-#' In this case, the returned object will also contain coordinates of a SPRING representation in its \code{\link{reducedDims}}.
+#' The cells used are specified in the \code{Used} column of the \code{\link{colData}}.
+#' 
+#' The \code{\link{reducedDim}} contains coordinates of SPRING representations.
+#' This may be filled with \code{NA}s for SPRING coordinates computed on a subset of cells (specified in \code{\link{colData}}).
 #'
 #' All data are downloaded from ExperimentHub and cached for local re-use.
 #' Specific resources can be retrieved by searching for \code{scRNAseq/zilionis-lung}.
@@ -28,7 +31,8 @@
 #'
 #' @references
 #' Zilionis R et al. (2019). 
-#' Single-Cell Transcriptomics of Human and Mouse Lung Cancers Reveals Conserved Myeloid Populations across Individuals and Species. 
+#' Single-cell transcriptomics of human and mouse lung cancers reveals 
+#' conserved myeloid populations across individuals and species. 
 #' \emph{Immunity} 50(5), 1317-1334.
 #'
 #' @examples
@@ -37,30 +41,41 @@
 #' sce.mouse <- ZilionisLungData("mouse")
 #' 
 #' @export
-#' @importFrom SummarizedExperiment colData
-#' @importFrom SingleCellExperiment reducedDim
+#' @importFrom SummarizedExperiment colData colData<-
+#' @importFrom SingleCellExperiment reducedDim<- reducedDims<- 
 ZilionisLungData <- function(which=c("human", "mouse"), ensembl=FALSE, location=TRUE, filter=FALSE) {
-  version <- "2.4.0"
-  which <- match.arg(which)
-  sce <- .create_sce(file.path("zilionis-lung", version), has.rowdata=FALSE, suffix=which)
-  
-  if(filter) {
-      # Subset to cells from the original analysis
-      keep <- !is.na(colData(sce)$Total.counts)
-      sce <- sce[, keep]
-      
-      # Add spring representation
-      which_x <- grep("^x_.*_all|^x$", colnames(colData(sce)))
-      which_y <- grep("^y_.*_all|^y$", colnames(colData(sce)))
-      spring_rep <- as.matrix(colData(sce)[, c(which_x, which_y)])
-      reducedDims(sce, "SPRING") <- spring_rep
+    version <- "2.4.0"
+    which <- match.arg(which)
+    sce <- .create_sce(file.path("zilionis-lung", version), has.rowdata=FALSE, suffix=which)
 
-      colData(sce) <- colData(sce)[,-c(which_x, which_y)]
-  }
-  
-  .convert_to_ensembl(sce, 
-                      ensembl=ensembl,
-                      species=if (which=="human") "Hs" else "Mm",
-                      symbols=rownames(sce),
-                      location=location)
+    if (filter) {
+        sce <- sce[,sce$Used]
+    }
+
+    cd <- colData(sce)
+    cn <- colnames(cd)
+    if (which=="human") {
+        which.x <- grep("^x_", cn)
+        which.y <- grep("^y_", cn)
+        grouping.x <- sub("^x_", cn[which.x])
+        grouping.y <- sub("^y_", cn[which.y])
+        stopifnot(identical(grouping.x, grouping.y)) # sanity check.
+
+        all.dims <- list()
+        for (i in seq_along(grouping.x)) {
+            all.dims[[paste0("SPRING_", grouping.x[i])]] <- cbind(x=cd[,which.x[i]], y=cd[,which.y[i]]) 
+        }
+        reducedDims(sce) <- all.dims
+    } else {
+        which.x <- which(cn=="x")
+        which.y <- which(cn=="y")
+        reducedDim(sce, "SPRING") <- cbind(x=cd[,which.x], y=cd[,which.y])
+    } 
+
+    colData(sce) <- colData(sce)[,-c(which_x, which_y)]
+    .convert_to_ensembl(sce, 
+        ensembl=ensembl,
+        species=if (which=="human") "Hs" else "Mm",
+        symbols=rownames(sce),
+        location=location)
 }
