5924840d203507f16df0425c476e642f39d7db23
Aaron Lun <LTLA@users.noreply.github.com>
2021-01-26 22:42:20 -0800

    Added the Bunis dataset. (#26)
    Co-authored-by: Daniel Bunis <daniel.bunis@ucsf.edu>

---
+++ inst/scripts/2.6.0/make-bunis-hspc-metadata.R
@@ -0,0 +1,25 @@
+write.csv(file="../../extdata/2.6.0/metadata-bunis-hspc.csv",
+    data.frame(
+        Title = sprintf("Bunis human HSPC %s", c("counts", "colData", "rowData")),
+        Description = sprintf("%s for the Bunis human haematopoietic stem-progenitor single-cell RNA-seq dataset", 
+            c("Count matrix", "Per-cell metadata", "Per-gene metadata")),
+        RDataPath = file.path("scRNAseq", "bunis-hspc", "2.6.0", 
+            c("counts.rds", "coldata.rds", "rowdata.rds")),
+        BiocVersion="3.13",
+        Genome="hg38",
+        SourceType=c("MTX", "TSV", "TSV"),
+        SourceUrl="https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE162086",
+        SourceVersion=c(
+            "GSE158490_matrix.mtx.gz",
+            "GSE158490_HSPC.best.txt.gz;GSE158490_barcodes.tsv.gz",	
+            "GSE158490_genes.tsv.gz"),
+        Species="Homo sapiens",
+        TaxonomyId="9606",
+        Coordinate_1_based=NA,
+        DataProvider="GEO",
+        Maintainer="Aaron Lun <infinite.monkeys.with.keyboards@gmail.com>",
+        RDataClass=c("dgCMatrix", "DFrame", "DFrame"),
+        DispatchClass="Rds",
+        stringsAsFactors = FALSE
+    ),
+    row.names=FALSE)
