77adf1d35e600510bd615f5d196500acf4bd0b26
vjcitn <stvjc@channing.harvard.edu>
2024-02-12 10:27:08 -0500

    support for README generation

--- R/process_bib.R
+++ R/process_bib.R
@@ -1,6 +1,3 @@
-#library(dplyr)
-#library(bibtex)
-#library(DT)
 
 wrap_pmid = function(x)
  sprintf("<a href='https://pubmed.ncbi.nlm.nih.gov/%s'>%s</a>", x, x)
@@ -10,8 +7,10 @@ unbrack = function (x)
 gsub("\\{|\\}", "", x)
 
 #' transform a bibtex file to data.frame
-#' bibfile character(1) path to bibtex
-#' has.pmids logical(1) if TRUE, href tags will be generated using the pmid attribute
+#' @import bibtex
+#' @import dplyr
+#' @param bibfile character(1) path to bibtex
+#' @param has.pmids logical(1) if TRUE, href tags will be generated using the pmid attribute
 #' of entries
 #' @return data.frame with rownames given by the bibtex tags
 #' @examples
@@ -32,18 +31,24 @@ bib_to_data.frame = function(bibfile, has.pmids=TRUE) {
 
 #datf = bib_to_data.frame("ref.bib")
 
-
-#' use DT::datatable to present information in listDatasets() with linked HTML for pubmed entries
+#' produce enriched data frame with pmid hrefs for studies in scRNAseq
 #' @export
-datasetsPage = function() {
+enhanceListDatasets = function() {
   bibref = system.file("scripts", "ref.bib", package="scRNAseq")
   bibdf = bib_to_data.frame("ref.bib")
   bibdf$tag = rownames(bibdf)
   ndf = as.data.frame(scRNAseq::listDatasets())
   ndf$tag = sub("@", "", ndf$Reference)
-  nndf = left_join(ndf, bibdf, by="tag")
+  nndf = dplyr::left_join(ndf, bibdf, by="tag")
   taxmap = c("8355"="Xenopus laevis", "9606"="Homo sapiens", "10090"="Mus musculus")
-  newdf = data.frame(org=taxmap[as.character(nndf$Taxonomy)],
+  data.frame(org=taxmap[as.character(nndf$Taxonomy)],
      part = nndf$Part, title=nndf$title, count=nndf$Number, pmid=nndf$pmid, call=nndf$Call)
+}
+
+#' use DT::datatable to present information in listDatasets() with linked HTML for pubmed entries
+#' @import DT
+#' @export
+datasetsPage = function() {
+  newdf = enhanceListDatasets()
   DT::datatable(newdf, escape=FALSE)
 }
