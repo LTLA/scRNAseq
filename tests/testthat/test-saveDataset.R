d5a64d1e1bc5353e5555ba65f0096cf5cc7becf0
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-02-22 09:20:21 -0800

    Added tests for correct link creation.

--- tests/testthat/test-saveDataset.R
+++ tests/testthat/test-saveDataset.R
@@ -31,3 +31,49 @@ test_that("saveDataset works as expected", {
     meta$title <- 1234
     expect_error(saveDataset(sce, tmp, meta), "title")
 })
+
+
+test_that("saveDataset works with ScrnaseqMatrix objects", {
+    meta <- list(
+        title="My dataset",
+        description="This is my dataset",
+        taxonomy_id="10090",
+        genome="GRCh38",
+        sources=list(list(provider="GEO", id="GSE12345")),
+        maintainer_name="Shizuka Mogami",
+        maintainer_email="mogami.shizuka@765pro.com"
+    )
+
+    # First attempt.
+    tmp <- tempfile()
+    saveDataset(sce, tmp, meta)
+
+    # Now decorating it with more gunk.
+    sce2 <- sce
+    assay(sce2, "foobar", withDimnames=FALSE) <- ScrnaseqArray(
+        name="test", 
+        version="foo", 
+        path="bar", 
+        cached=file.path(tmp, "assays", "0"),
+        seed=matrix(0, 100, 10)
+    )
+
+    assay(sce2, "blah", withDimnames=FALSE) <- ScrnaseqArray(
+        name="test", 
+        version="foo", 
+        path=NULL, 
+        cached=file.path(tmp, "assays", "0"),
+        seed=matrix(0, 100, 10)
+    )
+
+    tmp2 <- tempfile()
+    saveDataset(sce2, tmp2, meta)
+    roundtrip <- alabaster.base::readObject(tmp2)
+    expect_identical(as.matrix(assay(roundtrip, 2)), assay(sce)) # all assays are just symlinked to the first save!
+    expect_identical(as.matrix(assay(roundtrip, 3)), assay(sce))
+
+    link.data <- jsonlite::fromJSON(file.path(tmp2, "assays", "1", "_link"), simplifyVector=FALSE)
+    expect_identical(link.data$path, "bar")
+    link.data <- jsonlite::fromJSON(file.path(tmp2, "assays", "2", "_link"), simplifyVector=FALSE)
+    expect_null(link.data$path)
+})
