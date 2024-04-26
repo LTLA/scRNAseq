c2eebaf5ac3bdcd620c7bccb5c869993efba6b80
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-04-25 17:11:00 -0700

    Switch to the new formatObjectMetadata function when saving datasets.

--- R/saveDataset.R
+++ R/saveDataset.R
@@ -42,7 +42,7 @@
 #' 
 #' @export
 #' @importFrom alabaster.base saveObject
-#' @importFrom gypsum fetchMetadataSchema validateMetadata
+#' @importFrom gypsum fetchMetadataSchema validateMetadata formatObjectMetadata
 #' @importMethodsFrom alabaster.sce saveObject
 #' @importFrom jsonlite toJSON
 saveDataset <- function(x, path, metadata) {
@@ -58,29 +58,13 @@ saveDataset <- function(x, path, metadata) {
     unlink(path, recursive=TRUE)
     alabaster.base::saveObject(x, path, ReloadedArray.reuse.files="symlink")
 
-    takane <- list(type = readObjectFile(path)$type)
+    takane <- formatObjectMetadata(x)
+    takane$type <- readObjectFile(path)$type
 
-    if (is(x, "SummarizedExperiment")) {
-        takane$summarized_experiment = list(
-           rows = nrow(x),
-           columns = ncol(x),
-           assays = I(assayNames(x)),
-           column_annotations = I(colnames(colData(x)))
-        )
-
-        if (is(x, "SingleCellExperiment")) {
-            takane$single_cell_experiment <- list(
-                reduced_dimensions = I(reducedDimNames(x)),
-                alternative_experiments = I(altExpNames(x))
-            )
-        }
-    } else if (is(x, "DataFrame")) {
-        takane$data_frame <- list(
-            rows=nrow(x),
-            column_names=I(colnames(x))
-        )
+    if (is.null(metadata$applications)) {
+        metadata$applications <- list()
     }
-    metadata$applications <- c(metadata$applications, list(takane=takane))
+    metadata$applications$takane <- takane
 
     # Second validation with the takane metadata.
     contents <- toJSON(metadata, pretty=4, auto_unbox=TRUE)
